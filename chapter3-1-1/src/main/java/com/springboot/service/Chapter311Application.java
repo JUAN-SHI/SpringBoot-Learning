ec34d372f2f5fc2a5f2d1f3bb59595a916b656c0
SJuan <1361643223@qq.com>
2019-04-26 20:48:46 +0800

    构建一个较为复杂的RESTful API以及单元测试

---
+++ chapter3-1-1/src/main/java/com/springboot/service/Chapter311Application.java
@@ -0,0 +1,12 @@
+package com.springboot.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter311Application.class, args);
+	}
+}
